44f8404b63c74159386baa75e7332497b691ea03
venci0003 <venci0003@gmail.com>
2024-01-24 23:56:23 +0200

    Updated: - AdInformationViewModel.cs - AdController.cs - Information.css - Layout.css
    Added Create page.

--- Xcelerate.Core/Models/Ad/AdInformationViewModel.cs
+++ Xcelerate.Core/Models/Ad/AdInformationViewModel.cs
@@ -2,48 +2,50 @@
 
 namespace Xcelerate.Core.Models.Ad
 {
-    public class AdInformationViewModel
-    {
-        public List<string> ImageUrls { get; set; } = null!;
+	public class AdInformationViewModel
+	{
+		public List<string> ImageUrls { get; set; } = null!;
 
-        public List<AccessoryViewModel> Accessories { get; set; } = null!;
+		public List<AccessoryViewModel> Accessories { get; set; } = null!;
 
 		public string Brand { get; set; } = null!;
 
-        public string Model { get; set; } = null!;
+		public string Model { get; set; } = null!;
 
-        public int Year { get; set; }
+		public int Year { get; set; }
 
-        public string Engine { get; set; } = null!; 
+		public string Engine { get; set; } = null!;
 
-        public ConditionEnum Condition { get; set; }
+		public ConditionEnum Condition { get; set; }
 
-        public EuroStandardEnum EuroStandard { get; set; }
+		public EuroStandardEnum EuroStandard { get; set; }
 
-        public FuelTypeEnum FuelType { get; set; }
+		public FuelTypeEnum FuelType { get; set; }
 
-        public ColourEnum Colour { get; set; }
+		public ColourEnum Colour { get; set; }
 
-        public TransmitionEnum Transmition { get; set; }
+		public TransmitionEnum Transmition { get; set; }
 
-        public DriveTrainEnum DriveTrain { get; set; }
+		public DriveTrainEnum DriveTrain { get; set; }
 
-        public decimal Weight { get; set; }
+		public decimal Weight { get; set; }
 
-        public int Mileage { get; set; }
+		public int Mileage { get; set; }
 
-        public decimal Price { get; set; }
+		public decimal Price { get; set; }
 
-        public BodyTypeEnum BodyType { get; set; }
+		public BodyTypeEnum BodyType { get; set; }
 
-        public string Manufacturer { get; set; } = null!;
+		public string Manufacturer { get; set; } = null!;
 
-        public string Description { get; set; }
+		public string Description { get; set; } = null!;
 
-        // You can include necessary properties from the Address entity
+		public string Address { get; set; } = null!;
 
-        // You can include necessary properties from the Ad entity
+		// You can include necessary properties from the Address entity
 
-        // You can include necessary properties from the User entity
-    }
+		// You can include necessary properties from the Ad entity
+
+		// You can include necessary properties from the User entity
+	}
 }
--- Xcelerate/Controllers/AdController.cs
+++ Xcelerate/Controllers/AdController.cs
@@ -2,66 +2,45 @@
 using Microsoft.EntityFrameworkCore;
 using Xcelerate.Core.Models.Ad;
 using Xcelerate.Infrastructure.Data;
-using Xcelerate.Infrastructure.Data.Models;
 
 namespace Xcelerate.Controllers
 {
-    public class AdController : Controller
-    {
-        private readonly XcelerateContext _dbContext;
+	public class AdController : Controller
+	{
+		private readonly XcelerateContext _dbContext;
 
-        public AdController(XcelerateContext dbContext)
-        {
-            _dbContext = dbContext;
-        }
+		public AdController(XcelerateContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
 
-        public IActionResult Index()
-        {
-            var cars = _dbContext.Cars
-       .Include(car => car.Engine)
-       .Include(car => car.Images)
-       .ToList();
+		public async Task<IActionResult> Index()
+		{
 
-            // Map Car entities to AdViewModel
-            var adViewModels = cars.Select(car => new AdPreviewViewModel
-            {
-                CarId = car.CarId,
-                ImageUrls = car.Images.Select(image => image.ImageUrl).ToList(),
-                Brand = car.Brand,
-                Model = car.Model,
-                Year = car.Year,
-                Engine = car.Engine.Model, 
-                Condition = car.Condition,
-                EuroStandard = car.EuroStandard,
-                FuelType = car.FuelType,
-                Price = car.Price,
-            }).ToList();
+			var cars = await _dbContext.Cars.Select(car => new AdPreviewViewModel
+			{
+				CarId = car.CarId,
+				ImageUrls = car.Images.Select(car => car.ImageUrl).ToList(),
+				Brand = car.Brand,
+				Model = car.Model,
+				Year = car.Year,
+				Engine = car.Engine.Model,
+				Condition = car.Condition,
+				EuroStandard = car.EuroStandard,
+				FuelType = car.FuelType,
+				Price = car.Price,
+			}).ToListAsync();
 
-            return View(adViewModels);
-        }
+			return View(cars);
+		}
 
 
-        public IActionResult Information(int? carId)
-        {
-            var car = _dbContext.Cars
-                .Include(c => c.Engine)
-                .Include(c => c.Images)
-                .Include(c => c.Manufacturer)
-                .Include(c => c.Address)
-                .Include(c => c.Ad)
-                .Include(c => c.User)
-                .Include(c => c.Accessories)
-                .FirstOrDefault(c => c.CarId == carId);
+		public async Task<IActionResult> Information(int? carId)
+		{
 
-            if (car == null)
-            {
-                // Handle the case where the car with the specified CarId is not found
-                return NotFound();
-            }
-
-            var adViewModel = new AdInformationViewModel
-            {
-                ImageUrls = car.Images.Select(image => image.ImageUrl).ToList(),
+			var car = await _dbContext.Cars.Where(c => c.CarId == carId).Select(car => new AdInformationViewModel
+			{
+				ImageUrls = car.Images.Select(image => image.ImageUrl).ToList(),
 				Accessories = car.Accessories.Select(accessory => new AccessoryViewModel
 				{
 					GpsTrackingSystem = accessory.GpsTrackingSystem,
@@ -112,29 +91,35 @@ namespace Xcelerate.Controllers
 					CoolingGlovebox = accessory.CoolingGlovebox,
 				}).ToList(),
 				Brand = car.Brand,
-                Model = car.Model,
-                Year = car.Year,
-                Engine = car.Engine.Model,
-                Condition = car.Condition,
-                EuroStandard = car.EuroStandard,
-                FuelType = car.FuelType,
-                Colour = car.Colour,
-                Transmition = car.Transmition,
-                DriveTrain = car.DriveTrain,
-                Weight = car.Weight,
-                Mileage = car.Mileage,
-                Price = car.Price,
-                BodyType = car.BodyType,
-                Manufacturer = car.Manufacturer.Name,
-                Description = car.Ad.CarDescription
-            };
+				Model = car.Model,
+				Year = car.Year,
+				Engine = car.Engine.Model,
+				Condition = car.Condition,
+				EuroStandard = car.EuroStandard,
+				FuelType = car.FuelType,
+				Colour = car.Colour,
+				Transmition = car.Transmition,
+				DriveTrain = car.DriveTrain,
+				Weight = car.Weight,
+				Mileage = car.Mileage,
+				Price = car.Price,
+				BodyType = car.BodyType,
+				Manufacturer = car.Manufacturer.Name,
+				Description = car.Ad.CarDescription
+			}).FirstOrDefaultAsync();
 
-            return View(adViewModel);
-        }
+			if (car == null)
+			{
+				// Handle the case where the car with the specified CarId is not found
+				return NotFound();
+			}
+
+			return View(car);
+		}
 
 		public IActionResult Create()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 	}
 }
