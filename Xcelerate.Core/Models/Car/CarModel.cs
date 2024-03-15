27db6d80811d24cc0e6eaed79326dadbbd3cb013
venci0003 <venci0003@gmail.com>
2024-03-16 00:27:02 +0200

    .

---
+++ Xcelerate.Core/Models/Car/CarModel.cs
@@ -0,0 +1,12 @@
+﻿using Xcelerate.Core.Models.Ad;
+
+namespace Xcelerate.Core.Models.Car
+{
+	using Xcelerate.Core.Models.Pager;
+	public class CarModel
+	{
+		public List<AdPreviewViewModel> Cars { get; set;} = new List<AdPreviewViewModel>();
+
+		public Pager Pager { get; set; }
+	}
+}
---
+++ Xcelerate.Core/Models/Pager/Pager.cs
@@ -0,0 +1,45 @@
+﻿using static Xcelerate.Common.ApplicationConstants;
+
+namespace Xcelerate.Core.Models.Pager
+{
+	public class Pager
+	{
+		public Pager(int totalItems, int currentPage)
+		{
+			Configure(totalItems, currentPage);
+		}
+
+		private void Configure(int totalItems, int currentPage)
+		{
+			int totalPages = (int)Math.Ceiling((decimal)totalItems / DefaultPageSize);
+			int startPage = Math.Max(1, currentPage - 3);
+			int endPage = Math.Min(startPage + 4, totalPages);
+
+			if (currentPage > endPage && endPage != 0)
+			{
+				currentPage = endPage;
+				startPage = Math.Max(1, currentPage - 3);
+				endPage = Math.Min(startPage + 3, totalPages);
+			}
+
+			TotalPages = totalPages;
+			CurrentPage = currentPage;
+			PageSize = DefaultPageSize;
+			StartPage = startPage;
+			EndPage = endPage;
+			TotalItems = totalItems;
+		}
+
+		public int TotalItems { get; private set; }
+
+		public int CurrentPage { get; private set; }
+
+		public int PageSize { get; private set; }
+
+		public int TotalPages { get; private set; }
+
+		public int StartPage { get; private set; }
+
+		public int EndPage { get; private set; }
+	}
+}
