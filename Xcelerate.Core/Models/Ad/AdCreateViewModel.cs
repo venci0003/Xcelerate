7a51521c0dbac25e37dae311cc88822df69fd667
venci0003 <venci0003@gmail.com>
2024-03-30 19:37:17 +0200

    Added validations to view models.

--- Xcelerate.Core/Models/Ad/AdCreateViewModel.cs
+++ Xcelerate.Core/Models/Ad/AdCreateViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
 using Xcelerate.Infrastructure.Data.Enums;
+using static Xcelerate.Common.EntityValidation;
 
 namespace Xcelerate.Core.Models.Ad
 {
@@ -21,12 +23,17 @@ namespace Xcelerate.Core.Models.Ad
 
 		public bool IsForSale { get; set; }
 
+		[Required(ErrorMessage = "Model name is required.")]
+		[StringLength(CarEntity.ModelNameMaxLength, ErrorMessage = "Model name must be between {2} and {1} characters.", MinimumLength = CarEntity.ModelNameMinLength)]
 		public string Model { get; set; } = null!;
 
 		public int Year { get; set; }
 
+		[Required(ErrorMessage = "Engine name is required.")]
+		[StringLength(EngineEntity.ModelNameMaxLength, ErrorMessage = "Engine name must be between {2} and {1} characters.", MinimumLength = EngineEntity.ModelNameMinLength)]
 		public string Engine { get; set; } = null!;
 
+		[Range(EngineEntity.HorsePowerMinValue, EngineEntity.HorsePowerMaxValue, ErrorMessage = "Horsepower must be between {1} and {2}.")]
 		public int HorsePower { get; set; }
 
 		public ConditionEnum Condition { get; set; }
@@ -41,16 +48,25 @@ namespace Xcelerate.Core.Models.Ad
 
 		public DriveTrainEnum DriveTrain { get; set; }
 
+		[Range((double)CarEntity.WeightMinValue, (double)CarEntity.WeightMaxValue, ErrorMessage = "Weight must be between {1} and {2}.")]
 		public decimal Weight { get; set; }
 
+		[Required(ErrorMessage = "Mileage value is required.")]
+		[Range(CarEntity.MileageMinValue, CarEntity.MileageMaxValue, ErrorMessage = "Mileage must be between {1}KM and {2}KM.")]
 		public int Mileage { get; set; }
 
+		[Required(ErrorMessage = "Price value is required.")]
+		[Range(CarEntity.PriceMinValue, CarEntity.PriceMaxValue, ErrorMessage = "Price must be between {1}$ and {2}$.")]
 		public int Price { get; set; }
 
 		public BodyTypeEnum BodyType { get; set; }
 
+		[Required(ErrorMessage = "Manufacturer name is required.")]
+		[StringLength(CarEntity.ManufacturerNameMaxLength, ErrorMessage = "Manufacturer name must be between {2} and {1} characters.", MinimumLength = CarEntity.ManufacturerNameMinLength)]
 		public string Manufacturer { get; set; } = null!;
 
+		[Required(ErrorMessage = "Description is required.")]
+		[StringLength(AdEntity.CarDescriptionMaxLength, ErrorMessage = "Description must be between {2} and {1} characters.", MinimumLength = AdEntity.CarDescriptionMinLength)]
 		public string CarDescription { get; set; }
 	}
 }
--- Xcelerate.Core/Models/Ad/AddressViewModel.cs
+++ Xcelerate.Core/Models/Ad/AddressViewModel.cs
@@ -1,9 +1,20 @@
-﻿namespace Xcelerate.Core.Models.Ad
+﻿using System.ComponentModel.DataAnnotations;
+using static Xcelerate.Common.EntityValidation.AddressEntity;
+
+namespace Xcelerate.Core.Models.Ad
 {
 	public class AddressViewModel
 	{
+		[Required(ErrorMessage = "Country name is required.")]
+		[StringLength(CountryNameMaxLength, ErrorMessage = "Country name must be between {2} and {1} characters.", MinimumLength = CountryNameMinLength)]
 		public string CountryName { get; set; } = null!;
+
+		[Required(ErrorMessage = "Town name is required.")]
+		[StringLength(TownNameMaxLength, ErrorMessage = "Town name must be between {2} and {1} characters.", MinimumLength = TownNameMinLength)]
 		public string TownName { get; set; } = null!;
+
+		[Required(ErrorMessage = "Street name is required.")]
+		[StringLength(StreetNameMaxLength, ErrorMessage = "Street name must be between {2} and {1} characters.", MinimumLength = StreetNameMinLength)]
 		public string StreetName { get; set; } = null!;
 	}
 }
