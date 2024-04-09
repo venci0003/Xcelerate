b3b66842a44504832d741af1e92c001cc1647279
venci0003 <venci0003@gmail.com>
2024-04-09 03:40:07 +0300

    Added comment about decision to split data in news generator just to use one text area.

--- Xcelerate/Areas/Admin/Controllers/AdminNewsController.cs
+++ Xcelerate/Areas/Admin/Controllers/AdminNewsController.cs
@@ -27,6 +27,8 @@ namespace Xcelerate.Areas.Admin.Controllers
 			string title = parts[0];
 			string content = parts[1];
 
+			//This split is used because I didn't want to use separate text areas, and I wanted the title and content to be in one text area.
+
 			generatedNewsView.Title = title;
 			generatedNewsView.Content = content;
 
