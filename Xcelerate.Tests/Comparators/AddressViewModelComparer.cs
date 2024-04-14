28d87bc337b67bef9f21126a5d3a09c3c8f03092
venci0003 <venci0003@gmail.com>
2024-04-14 19:58:38 +0300

    Added address view model comparer.

---
+++ Xcelerate.Tests/Comparators/AddressViewModelComparer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using Xcelerate.Core.Models.Ad;
+
+namespace Xcelerate.Tests.Comparators
+{
+	public class AddressViewModelComparer : IComparer
+	{
+		public int Compare(object? x, object? y)
+		{
+			AddressViewModel address1 = (AddressViewModel)x;
+			AddressViewModel address2 = (AddressViewModel)y;
+
+			if (address1 == null || address2 == null)
+			{
+				return -1;
+			}
+
+			if (address1.StreetName != address2.StreetName ||
+				address1.CountryName != address2.CountryName ||
+				address1.TownName != address2.TownName)
+			{
+				return -1;
+			}
+
+			return 0;
+		}
+	}
+}
