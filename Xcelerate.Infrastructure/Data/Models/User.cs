35e0400d4e96db17446aa873e0fa90e0a134a4fe
venci0003 <venci0003@gmail.com>
2024-04-27 14:27:37 +0300

    .

--- Xcelerate.Infrastructure/Data/Models/User.cs
+++ Xcelerate.Infrastructure/Data/Models/User.cs
@@ -5,20 +5,22 @@ using System.ComponentModel.DataAnnotations;
 namespace Xcelerate.Infrastructure.Data.Models
 {
 	public class User : IdentityUser<Guid>
-    {
+	{
 
-        [Required]
-        [Comment("FirstName")]
-        public string FirstName { get; set; } = null!;
+		[Required]
+		[Comment("FirstName")]
+		public string FirstName { get; set; } = null!;
 
-        [Required]
-        [Comment("LastName")]
-        public string LastName { get; set; } = null!;
+		[Required]
+		[Comment("LastName")]
+		public string LastName { get; set; } = null!;
 
 		public ICollection<Ad> Ads { get; set; } = new List<Ad>();
 
 		public ICollection<Review> Reviews { get; set; } = new List<Review>();
 
 		public ICollection<Car> Cars { get; set; } = new List<Car>();
-    }
+
+		public ICollection<Message> Messages { get; set; } = new List<Message>();
+	}
 }
