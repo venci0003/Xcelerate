22803a332113c6a43efcea0eec4164942250241d
venci0003 <venci0003@gmail.com>
2024-05-02 23:37:31 +0300

    Added notification messages constants.

---
+++ Xcelerate.Common/NotificationMessages.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xcelerate.Common
+{
+	public class NotificationMessages
+	{
+		public static class UserMessages
+		{
+			//Create messages
+			public const string SuccesfulCreateTitle = "Succesfully added new ad!";
+
+			public const string SuccesfulCreateContent = "Car added: {0} - {1} {2}";
+
+			//Edit messages
+			public const string SuccesfulEditTitle = "Succesfully edited car ad!";
+
+			public const string SuccesfulEditContent = "Car edited: {0} - {1} {2}";
+
+			//Delete messages
+			public const string SuccesfulDeleteTitle = "Succesfully deleted car ad!";
+
+			public const string SuccesfulDeleteContent = "Car deleted: {0} - {1} {2}";
+		};
+	}
+}
